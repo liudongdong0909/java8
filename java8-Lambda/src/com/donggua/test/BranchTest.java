31a38679021bedd5f559a5065d449a328b2c5e4b
liudongdong0909 <liudongdong0909@yeah.net>
2018-02-27 10:26:54 +0800

    Revert: delete test branch

---
+++ java8-Lambda/src/com/donggua/test/BranchTest.java
@@ -0,0 +1,16 @@
+package com.donggua.test;
+
+/**
+ * @author IT_donggua
+ * @version V1.0
+ * @create 2018-02-27 上午 10:13
+ */
+public class BranchTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git branch test!");
+    }
+}
+
+
+
